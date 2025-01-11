e1d19b9cb297f9dccd8e197196c35495c975ad28
Eduk <ekakhrisnanda@gmail.com>
2025-01-11 14:35:02 +0800

    Map the operators inside operate

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('.display-container');
 let initNum;
-let operator;
 let nextNum;
 
 const operate = (a, b, operator) => {
-    switch(operator) {
-        case '+':
-            add(a, b);
-            break;
-        case '-':
-            substract(a, b);
-            break;
-        case 'x':
-            multiply(a, b);
-            break;
-        case '/':
-            divide(a, b);
-    }
+    operators.map(operator => {
+        switch(operator) {
+            case '+':
+                add(a, b);
+                break;
+            case '-':
+                substract(a, b);
+                break;
+            case 'x':
+                multiply(a, b);
+                break;
+            case '/':
+                divide(a, b);
+        };
+    });
 }
