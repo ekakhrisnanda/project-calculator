85e2ef88141ca848b6e2fb883e407eca3260a443
Eduk <ekakhrisnanda@gmail.com>
2025-01-11 16:07:09 +0800

    Create displayValue function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+
+const buttons = document.querySelectorAll('.button')
 const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('.display-container');
 let initNum;
@@ -26,3 +28,22 @@ const operate = (a, b, operators) => {
     }
 };
 
+const displayValue = () => {
+    buttons.forEach(button => {
+        let val = button.value
+        button.addEventListener('click', (e) => {
+           let val = e.target.value;
+           if(e.target.classList.contains('operator') || e.target.classList.contains('number')) {
+            display.textContent = val;
+           } 
+        });
+    });
+};
+
+displayValue()
+
+
+
+// if a number button pressed stores the value in the initNum variable
+// if an operator buttons pressed store the values and prohibit to add another operator
+// if a number after operator pressed stores the value in the nextNum variable
